2dd60ce3d5178b2bffce51dd58cc7c0bee855944
Chaima <1925046@alu.murciaeduca.es>
2024-03-07 19:05:56 +0100

    cambio de nombre de agenda

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Crear una nueva agendaContactos.
-        initAgenda();
+        initAgenda(); //funcion que me he creado
 
 
     }
